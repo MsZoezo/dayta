7710520cb943d11d07dbc261f190605f51d9bc18
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-05-14 13:00:50 -0500

    Add Calendar layout

---
+++ src/layouts/CalendarViewLayout.tsx
@@ -0,0 +1,15 @@
+import ControlPanel from "../ControlPanel/ControlPanel";
+import "./CalendarViewLayout.css"
+
+function CalendarLayout() {
+    return (
+        <div className="Layout">
+            <ControlPanel/>
+            <main>
+                <h1>This is the calendar</h1>
+            </main>
+        </div>
+    )
+}
+
+export default CalendarLayout;
