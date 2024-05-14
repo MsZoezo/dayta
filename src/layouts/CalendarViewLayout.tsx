7685fa1323d379e261f49d462b9900669ce9f70b
MsZoezo <zoey@zoezo.dev>
2024-05-14 23:14:28 +0200

    Remove testing code for colour scheme

--- src/layouts/CalendarViewLayout.tsx
+++ src/layouts/CalendarViewLayout.tsx
@@ -7,8 +7,6 @@ function CalendarLayout() {
             <ControlPanel/>
             <main>
                 <h1>This is the calendar</h1>
-                <a href="/">Testing a link</a><br/>
-                <a href="/" className="disabled">Testing a 'disabled' link</a>
             </main>
         </div>
     )
