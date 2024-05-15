1fedf4b0eaaeb78f89b99717bff69d344d5f4a6a
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-05-15 13:00:25 -0500

    Add a shared component for the day labels

---
+++ src/CalendarViews/Components/DayLabels/DayLabels.tsx
@@ -0,0 +1,27 @@
+import "../../SharedStyling/WeekFormating.css"
+
+function DayLabels(){
+    const maxDays = 7; //JS days go from 0-6
+
+    const dayLabels = Array.from(Array(maxDays).keys()).map(GetDayLabel)
+    return (
+        <div className="weekFormat">
+            {dayLabels}
+        </div>
+    )
+}
+
+function GetDayName(dayIdx: number):string {
+    const baseDate = 1
+    const workingDate = new Date(1970, 2)
+    workingDate.setDate(baseDate + dayIdx)
+    return workingDate.toLocaleDateString(undefined, {weekday:"long"})
+}
+
+function GetDayLabel(dayIdx: number): JSX.Element {
+    return <div className="DayLabel" key={dayIdx}>
+        <p>{GetDayName(dayIdx)}</p>
+    </div>
+}
+
+export default DayLabels
