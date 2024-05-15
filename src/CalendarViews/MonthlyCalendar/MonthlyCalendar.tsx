a6af6ebb99f1c29425896ae3b40b5afd56296c50
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-05-15 13:02:14 -0500

    Use components for the basic calendar body and the day labels

--- src/CalendarViews/MonthlyCalendar/MonthlyCalendar.tsx
+++ src/CalendarViews/MonthlyCalendar/MonthlyCalendar.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import CalendarHeader from "./Header/CalendarHeader"
+import CalendarBody from "./MainBody/MainBody";
+import DayLabels from "../Components/DayLabels/DayLabels";
 
 function MonthlyCalandar() {
     let [date, setDate] = useState(new Date())
@@ -23,6 +25,8 @@ function MonthlyCalandar() {
     return (
         <>
             <CalendarHeader date={date} changeMonthYear={ChangeMonth} />
+            <DayLabels/>
+            <CalendarBody date={date}/>
         </>
     )
 }
