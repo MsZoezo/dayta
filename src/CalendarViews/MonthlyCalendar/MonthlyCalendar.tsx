7b96ad8f6e178c3fbaf3d4eaa0376940ad381d59
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-05-15 13:07:05 -0500

    Don't worry about handling overflow, that's already handled by date

--- src/CalendarViews/MonthlyCalendar/MonthlyCalendar.tsx
+++ src/CalendarViews/MonthlyCalendar/MonthlyCalendar.tsx
@@ -7,18 +7,7 @@ function MonthlyCalandar() {
     let [date, setDate] = useState(new Date())
     
     function ChangeMonth(offset: number){
-        const maxMonth = 11;
-        let updatedYear = date.getFullYear();
-        let updatedMonth = date.getMonth() + offset;
-
-        if (updatedMonth > maxMonth) {
-            updatedMonth = 0
-            updatedYear++
-        } else if (updatedMonth < 0) {
-            updatedMonth = maxMonth
-            updatedYear--
-        }
-        setDate(new Date(updatedYear, updatedMonth))
+        setDate(new Date(date.getFullYear(), date.getMonth() + offset))
 
     }
     
